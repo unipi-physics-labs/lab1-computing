d83027b9cbe14f0770e71a946e70d57f14d86787
Luca Baldini <luca.baldini@pi.infn.it>
2026-08-22 09:24:25 +0200

    Release script modified, now generating the README file

--- tools/release.py
+++ tools/release.py
@@ -31,23 +31,47 @@ from loguru import logger
 
 # Configure the logger.
 logger.remove()
-logger.add(sink=sys.stderr, colorize=True, format='>>> <level>{message}</level>')
+logger.add(sink=sys.stderr, colorize=True, format=">>> <level>{message}</level>")
 
 
 REPO_ROOT = pathlib.Path(__file__).resolve().parent.parent
-METADATA_FILE_PATH = REPO_ROOT / 'meta.yml'
-README_FILE_PATH = REPO_ROOT / 'README.md'
-GITHUB_RELEASE_URL = 'https://github.com/unipi-physics-labs/lab1-computing/releases'
+METADATA_FILE_PATH = REPO_ROOT / "meta.yml"
+README_FILE_PATH = REPO_ROOT / "README.md"
+GITHUB_PAGES_URL = "https://unipi-physics-labs.github.io/lab1-computing/"
+GITHUB_PAGES_URL = "https://github.com/unipi-physics-labs/lab1-computing/issues"
+GITHUB_RELEASE_URL = "https://github.com/unipi-physics-labs/lab1-computing/releases"
 
 INCREMENT_MODES = ('major', 'minor', 'patch')
 
 _ENCODING = 'utf-8'
 
+README = f"""# lab1-computing
 
-def execute_shell_command(arguments):
+Materiale didattico a supporto del modulo di computazione.
+
+La versione corrente delle note è disponibile:
+
+* in formato [html]({GITHUB_PAGES_URL})
+(se avete accesso alla rete questa è la modalità di consultazione consigliata);
+* in formato [pdf](%s)
+(le animazioni non funzioneranno, ma il contenuto è rigorosamente lo stesso).
+
+Se trovate errori e/o omissioni, oppure avete suggerimenti per migliorare il materiale,
+non esitate ad aprire una issue nel nostro
+[tracker]({GITHUB_PAGES_URL}).
+
+Buona lettura!
+"""
+
+
+
+def execute_shell_command(arguments, dry_run: bool = False):
     """Execute a shell command.
     """
     logger.info(f'About to execute "{" ".join(arguments)}"...')
+    if dry_run:
+        logger.info("Dry run: command not executed.")
+        return None
     return subprocess.run(arguments, check=True)
 
 def _read_metadata():
@@ -78,8 +102,7 @@ def _write_readme(metadata: dict) -> None:
     readme_path = REPO_ROOT / "README.md"
     logger.info(f"Writing README file to {readme_path}...")
     with open(readme_path, "w", encoding=_ENCODING) as readme_file:
-        readme_file.write("# lab1-computing\n\n")
-        readme_file.write("Materiale didattico a supporto del modulo di computazione.\n")
+        readme_file.write(README % _asset_url("compnotes", metadata['tag']))
 
 def _update_history(metadata: dict) -> None:
     """ Update the history file.
@@ -99,45 +122,47 @@ def _update_history(metadata: dict) -> None:
 def bump_version(mode: str) -> str:
     """Update the version.tex file.
     """
-    logger.info(f'Bumping version (mode = {mode})...')
-    old_version = _read_metadata().get('tag')
-    major, minor, patch = (int(item) for item in old_version.split('.'))
-    if mode == 'major':
+    logger.info(f"Bumping version (mode = {mode})...")
+    old_version = _read_metadata().get("tag")
+    major, minor, patch = (int(item) for item in old_version.split("."))
+    if mode == "major":
         major += 1
         minor = 0
         patch = 0
-    elif mode == 'minor':
+    elif mode == "minor":
         minor += 1
         patch = 0
-    elif mode == 'patch':
+    elif mode == "patch":
         patch += 1
     new_version = f'{major}.{minor}.{patch}'
-    logger.info(f'Target version is {new_version}')
+    logger.info(f"Target version is {new_version}")
     metadata = dict(tag=new_version, date=date.today().isoformat())
     _write_metadata(metadata)
     _write_readme(metadata)
     _update_history(metadata)
     return metadata
 
-def release(mode: str) -> None:
+def release(mode: str, dry_run: bool = False) -> None:
     """ Tag the package and create a release.
     """
-    execute_shell_command(['git', 'pull'])
+    execute_shell_command(["git", "pull"], dry_run)
     meta = bump_version(mode)
     version = meta['tag']
     msg = f'Prepare for tag {version}.'
-    execute_shell_command(['git', 'commit', '-a', '-m', msg])
-    execute_shell_command(['git', 'push'])
+    execute_shell_command(["git", "commit", "-a", "-m", msg], dry_run)
+    execute_shell_command(["git", "push"], dry_run)
     msg = f'Tagging version {version}...'
-    execute_shell_command(['git', 'tag', '-a', version, '-m', msg])
-    execute_shell_command(['git', 'push', '--tags'])
-    execute_shell_command(['git', 'status'])
+    execute_shell_command(["git", "tag", "-a", version, "-m", msg], dry_run)
+    execute_shell_command(["git", "push", "--tags"], dry_run)
+    execute_shell_command(["git", "status"], dry_run)
     logger.info(f'Release {version} completed successfully.')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('mode', type=str, choices=INCREMENT_MODES,
-                        help='Version increment mode')
+    parser.add_argument("mode", type=str, choices=INCREMENT_MODES,
+                        help="Version increment mode")
+    parser.add_argument('--dry-run', action='store_true',
+                        help="Perform a dry run without issuing git commands")
     args = parser.parse_args()
-    release(args.mode)
+    release(args.mode, dry_run=args.dry_run)
